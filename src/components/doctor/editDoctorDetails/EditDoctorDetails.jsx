53e47190f14aa2081ee4b271d670d94ba2e86f55
Kundan Prasad Yadav <110804293+kundanYadav1@users.noreply.github.com>
2025-05-27 09:27:56 +0530

    Update EditDoctorDetails.jsx

--- src/components/doctor/editDoctorDetails/EditDoctorDetails.jsx
+++ src/components/doctor/editDoctorDetails/EditDoctorDetails.jsx
@@ -15,7 +15,7 @@ const EditDoctorDetails = () => {
     const getUserData = async (e) => {
         try {
             const token = localStorage.getItem("authToken")
-            const details = await axios.get(`https://kritaahospital.netlify.apprender.com/getuserdetails`, {
+            const details = await axios.get(`https://doctorapp-backend-gilt.vercel.app/getuserdetails`, {
                 headers: {
                     'Authorization': `${token}`
                 }
@@ -45,7 +45,7 @@ const EditDoctorDetails = () => {
                 profileData.append(key, doctorDetails[key]);
             }
             const token = localStorage.getItem('authToken');
-            const response = await axios.patch(`https://kritaahospital.netlify.apprender.com/updateuserdetails`, profileData, {
+            const response = await axios.patch(`https://doctorapp-backend-gilt.vercel.app/updateuserdetails`, profileData, {
                 headers: {
                     'Authorization': `${token}`
                 }
@@ -59,7 +59,7 @@ const EditDoctorDetails = () => {
 
     return (
         <div className='editdoctordetails'>
-            <div className='editdoctorimage'><img className='editdoctorprofileimage' src={`https://kritaahospital.netlify.apprender.com/`+doctorDetails.profile_image} alt='doctor'></img></div>
+            <div className='editdoctorimage'><img className='editdoctorprofileimage' src={`https://doctorapp-backend-gilt.vercel.app/`+doctorDetails.profile_image} alt='doctor'></img></div>
             <input className='doctorlabelinputprofile' type="file" accept=".jpg, .jpeg, .png" onChange={onChangeImage} />
             <div className='editdoctordata'>
                 <div className='editdoctordataleft'>
